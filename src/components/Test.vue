82f790dda4808859ed98c723647507b1cd9fddd4
scaler <zengkaiwang@yeah.net>
2021-04-15 08:24:07 +0800

    setup两种语法对比

---
+++ src/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <div>{{ readersNumber }} {{ book.title }}</div>
+
+  <!-- <button @click="state.count++">count is: {{ state.count }}</button> -->
+</template>
+
+<script>
+import { defineProps, reactive, ref } from "vue";
+
+export default {
+  setup() {
+    const readersNumber = ref(0);
+    const book = reactive({ title: "Vue 3 Guide" });
+
+    // expose to template
+    return {
+      readersNumber,
+      book,
+    };
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
